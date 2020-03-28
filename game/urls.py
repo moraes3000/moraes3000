505ef28a14bb2a36abfd6eec790b4094e2d7676e
moraes3000 <bruno_bmoraes@hotmail.com>
2020-03-28 13:25:41 -0300

    crud add jogo-admin

--- game/urls.py
+++ game/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from game.views.capitulos import CapituloJogoListView, CapituloJogoDetailView
-from game.views.jogos import JogoListView, JogoCreateView,AdminJogoList
+from game.views.jogos import JogoListView, JogoCreateView, AdminJogoList, JogoUpdate, JogoDelete
 
 urlpatterns = [
     path('', JogoListView.as_view(), name='JogoListView'),
     # admin
     path('novo', JogoCreateView.as_view(), name='JogoCreateView'),
     path('admin-listar-jogo', AdminJogoList.as_view(), name='AdminJogoList'),
+    path('admin-editar-jogo/<slug:slug>',JogoUpdate.as_view(), name='JogoUpdate'),
+    path('admin-remover-jogo/<slug:slug>', JogoDelete.as_view(), name='JogoDelete'),
 
     # Capitulos
     path('lista-de-capitulos/<int:pk>/', CapituloJogoListView.as_view(), name='CapituloJogoListView'),
--- game/views/jogos.py
+++ game/views/jogos.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView,UpdateView
-from django.urls import reverse_lazy
+from django.views.generic import ListView,DetailView,CreateView,UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 from  game.forms import JogoForm
 
 from game.models import JogoModel
@@ -15,10 +15,35 @@ class JogoCreateView(CreateView):
     model = JogoModel
     form_class = JogoForm
     template_name = 'jogo/adicionar.html'
-
-    success_url = reverse_lazy()
+    success_url = reverse_lazy('AdminJogoList')
 
 class AdminJogoList(ListView):
     model = JogoModel
     template_name = 'jogo/admin-lista.html'
+
+
+class JogoUpdate(UpdateView):
+    model = JogoModel
+    form_class = JogoForm
+    template_name = 'jogo/adicionar.html'
+    success_url = reverse_lazy('AdminJogoList')
+
+    # def get_success_url(self):
+    #     return reverse('AdminJogoList', kwargs={})
+
+
+class JogoDelete(DeleteView):
+    model = JogoModel
+    form_class = JogoForm
+    template_name = 'jogo/excluir.html'
+    success_url = reverse_lazy('AdminJogoList')
+
+
+
+
+#
+# class RequisicaoDeNumerariosDeleteView(DeleteView):
+#     model = RequisicaoDeNumerariosModel
+#     template_name = 'requisaodenumerarios/requisicao-de-numerarios/excluir.html'
+#     success_url = reverse_lazy('formulariosbasicos:pagamento-listar')
 
